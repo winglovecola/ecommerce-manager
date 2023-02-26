3eb05ea5beb812278aae55a48283f0ce92efb940
winglovecola@gmail.com <winglovecola@gmail.com>
2023-02-26 01:46:15 -0500

    bug fix for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
 
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category }],
+      include: [{ model: Category }, { model: Tag }],
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -26,7 +26,7 @@ router.get('/:id', async (req, res) => {
 
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }],
+      include: [{ model: Category }, { model: Tag }],
     });
 
     if (!productData) {
